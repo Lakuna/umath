f6385f41a504d3e2c77311e28ca6faa841d779c2
Travis Martin <tjmartin2003@gmail.com>
2023-08-03 00:12:05 -0500

    Add the PMF for the hypergeometric distribution.

--- src/algorithms/factorial.ts
+++ src/algorithms/factorial.ts
@@ -5,5 +5,9 @@
  * @see [Factorial](https://en.wikipedia.org/wiki/Factorial)
  */
 export default function factorial(n: number): number {
-	return n == 0 ? 1 : n * factorial(n - 1);
+	return n < 0
+		? Infinity * (n % 2 ? 1 : -1) // Division by zero returns `Infinity` in JavaScript.
+		: n == 0
+			? 1
+			: n * factorial(n - 1);
 }
